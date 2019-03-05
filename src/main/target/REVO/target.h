0828c10ed7082eaaa1e078bf4edc7d04a6e9f928
elin-neo <neodrone@neosystems.co.kr>
2019-03-05 16:37:02 +0900

    remove a timer for PC6.

--- src/main/target/REVO/target.h
+++ src/main/target/REVO/target.h
@@ -356,7 +356,7 @@
 #define USABLE_TIMER_CHANNEL_COUNT 13
 #define USED_TIMERS             ( TIM_N(1) | TIM_N(2) | TIM_N(3) | TIM_N(5) | TIM_N(8) | TIM_N(12) )
 #elif defined(ELINF405) || defined(ELINF722)
-#define USABLE_TIMER_CHANNEL_COUNT 10
+#define USABLE_TIMER_CHANNEL_COUNT 9
 #define USED_TIMERS             ( TIM_N(1) | TIM_N(2) | TIM_N(3) | TIM_N(4) | TIM_N(8) | TIM_N(11) )
 #else
 #define USABLE_TIMER_CHANNEL_COUNT 12
